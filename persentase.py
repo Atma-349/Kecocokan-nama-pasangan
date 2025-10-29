87ec7402af48b039ed94933e0cedcf8aa6251af5
Atma-349 <muhammadimamarsyad@gmail.com>
2025-10-29 22:22:25 +0700

    first commit

---
+++ persentase.py
@@ -0,0 +1,11 @@
+import random
+
+def random_similarity():
+    return random.uniform( 0,100) 
+
+if __name__ == "__main__":
+    nama1 = input("Masukkan nama pertama: ")
+    nama2 = input("Masukkan nama kedua: ")
+
+    similarity_percent = random_similarity()
+    print(f"Tingkat kecocokan: {similarity_percent:.2f}%")
